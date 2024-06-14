359d5a869a8a0a1bdf5cfe1fe01ba8a39e0f0c6b
Fizikaman <fizikaman26@gmail.com>
2024-06-14 15:17:15 +0300

    Модель User

--- user_app/models.py
+++ user_app/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    first_name = models.CharField(max_length=255, verbose_name="Имя")
+    last_name = models.CharField(max_length=255, verbose_name="Фамилия")
+    middle_name = models.CharField(max_length=255, null=True, blank=True, verbose_name="Отчество")
+    birth_date = models.DateField(verbose_name="Дата рождения")
+    passport_number = models.CharField(max_length=15, verbose_name="Номер паспорта")
+    place_of_birth = models.CharField(max_length=255, verbose_name="Место рождения")
+    phone = models.CharField(
+        max_length=11, verbose_name="Телефон",
+        validators=[RegexValidator(regex='^7[0-9]{10}$', message='Введите правильный российский номер телефона.')])
+    email = models.EmailField(null=True, blank=True, verbose_name="Email")
+    registration_address = models.CharField(max_length=255, verbose_name="Адрес регистрации")
+    residential_address = models.CharField(max_length=255, null=True, blank=True, verbose_name="Адрес проживания")
+
+    def __str__(self):
+        return f"{self.last_name} {self.first_name}"
